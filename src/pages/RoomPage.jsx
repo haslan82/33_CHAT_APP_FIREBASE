97cda3b34909373566c64681d9a120b9d05a1260
haslan82 <haslan82@hotmail.com>
2024-07-03 15:58:29 +0300

    ekledim

--- src/pages/RoomPage.jsx
+++ src/pages/RoomPage.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const RoomPage = () => {
   return (
-    <div>
-   Oda Seçme
-    </div>
+    <form className='room-page'>
+      <h1>Chat Odası</h1>
+      <p>Hangi Odaya Gireceksiniz ?</p>
+      <input placeholder='Ör: Haftaiçi' type="text" />
+      <button type='submit'>Odaya Gir</button>
+      <button type='button'>Çıkış Yap</button>
+    </form>
   )
 }
 
